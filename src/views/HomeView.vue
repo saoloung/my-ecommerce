9d59e3e42fc0fa8b9cd34e24c473e1157f645ef7
Champ <saoloung@gmail.com>
2023-12-11 16:05:20 +0700

    add UserLayout Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
+<script setup>
+import UserLayout from '@/layouts/UserLayout.vue'
+</script>
+
 <template>
-    <div>
-        Home View
-    </div>
-    <button class="btn">Button</button>
-    <button class="btn btn-neutral">Neutral</button>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-accent">Accent</button>
-    <button class="btn btn-ghost">Ghost</button>
-    <button class="btn btn-link">Link</button>
+    <UserLayout>
+        Content Data
+    </UserLayout>
 </template>
